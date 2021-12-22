14510d5ea8825f09eb9848849f0a4c7980ccba72
ltoddy <taoliu0509@gmail.com>
2021-12-22 10:55:58 +0800

    chore: simplify code

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,6 @@
-use std::ffi::{CStr, OsString};
+use std::env;
 use std::fs;
-use std::mem;
-use std::os::unix::prelude::*;
 use std::path::{Path, PathBuf};
-use std::ptr;
 
 pub type Result<T, E = Box<dyn std::error::Error>> = std::result::Result<T, E>;
 
@@ -26,50 +23,5 @@ fn setup_impl(command: &str, filename: &Path) -> Result<()> {
 }
 
 fn home_dir() -> Option<PathBuf> {
-    return std::env::var_os("HOME")
-        .or_else(|| unsafe { fallback() })
-        .map(PathBuf::from);
-
-    #[cfg(any(
-        target_os = "android",
-        target_os = "ios",
-        target_os = "emscripten",
-        target_os = "redox",
-        target_os = "vxworks",
-        target_os = "espidf"
-    ))]
-    unsafe fn fallback() -> Option<OsString> {
-        None
-    }
-    #[cfg(not(any(
-        target_os = "android",
-        target_os = "ios",
-        target_os = "emscripten",
-        target_os = "redox",
-        target_os = "vxworks",
-        target_os = "espidf"
-    )))]
-    unsafe fn fallback() -> Option<OsString> {
-        let amt = match libc::sysconf(libc::_SC_GETPW_R_SIZE_MAX) {
-            n if n < 0 => 512_usize,
-            n => n as usize,
-        };
-        let mut buf = Vec::with_capacity(amt);
-        let mut passwd: libc::passwd = mem::zeroed();
-        let mut result = ptr::null_mut();
-        match libc::getpwuid_r(
-            libc::getuid(),
-            &mut passwd,
-            buf.as_mut_ptr(),
-            buf.capacity(),
-            &mut result,
-        ) {
-            0 if !result.is_null() => {
-                let ptr = passwd.pw_dir as *const _;
-                let bytes = CStr::from_ptr(ptr).to_bytes().to_vec();
-                Some(OsStringExt::from_vec(bytes))
-            }
-            _ => None,
-        }
-    }
+    env::var_os("HOME").map(PathBuf::from)
 }
