f757eaa9028c86645a89a3fd6e242a86e6282a1f
ltoddy <taoliu0509@gmail.com>
2021-12-23 09:49:38 +0800

    optimize: remove result

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,17 @@
 use std::env;
 use std::fs;
+use std::io;
 use std::path::{Path, PathBuf};
 
-pub type Result<T, E = Box<dyn std::error::Error>> = std::result::Result<T, E>;
-
-pub fn setup<P>(command: &str, filename: P) -> Result<()>
+pub fn setup<P>(command: &str, filename: P) -> io::Result<()>
 where
     P: AsRef<Path>,
 {
     setup_impl(command, filename.as_ref())
 }
 
-fn setup_impl(command: &str, filename: &Path) -> Result<()> {
-    let home_directory = home_dir().unwrap_or_else(|| PathBuf::from("."));
+fn setup_impl(command: &str, filename: &Path) -> io::Result<()> {
+    let home_directory = home_dir().expect("can't find $HOME directory");
     let man_directory = home_directory.join(".local/share/man/man1");
     if !man_directory.is_dir() {
         fs::create_dir_all(&man_directory)?;
@@ -23,6 +22,7 @@ fn setup_impl(command: &str, filename: &Path) -> Result<()> {
     Ok(())
 }
 
+#[inline]
 fn home_dir() -> Option<PathBuf> {
     env::var_os("HOME").map(PathBuf::from)
 }
